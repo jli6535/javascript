25eba8496a6b482cec8d161ae41b212478e6edbb
jli6535 <86691138+jli6535@users.noreply.github.com>
2021-07-08 12:24:36 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C C D D E F G G F F E D C C", 120)
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+
+
